032b73f10b7a8008789192323e9764ad34cb1afb
jotapecds <jpcoelhodesouza@gmail.com>
2023-11-03 19:52:08 -0300

    Commit CDC changes

--- myFinancingSimulator/scripts/price.js
+++ myFinancingSimulator/scripts/price.js
@@ -2,18 +2,17 @@
 
 let params = new URLSearchParams(window.location.search);
 
+let NP = params.get("np");
+let TAX = params.get("tax");
+let PV = params.get("pv");
+let PP = params.get("pp");
+let PB = params.get("pb");
+let existeEntrada = params.get("dp") != null ? true : false;
+
 let priceTableData = null;
 
-if(params.get("np") != null && 
-    params.get("tax") != null && 
-    params.get("pv") != null && 
-    params.get("pp") != null && 
-    params.get("pb") != null) {
-
-    // O parâmetro "dp" indica se existe entrada, e é opcional.
-    let existeEntrada = params.get("dp") != null ? true : false;
-
-    priceTableData = calculatePriceTable(params.get("np"), params.get("tax"), params.get("pv"), params.get("pp"), params.get("pb"), existeEntrada);
+if(NP != null && TAX != null && PV != null && PP != null) {
+    priceTableData = calculatePriceTable(NP, PV, PP, TAX, existeEntrada);
 } 
 else {
     throw new UserException("DadosInvalidos");
@@ -21,11 +20,38 @@ else {
 
 /* --- Gerando a tabela dinamicamente --- */
 
-const table = document.getElementById("price-table");
+const tableElement = document.getElementById("price-table");
 
 priceTableData.forEach(rowData => {
-    table.appendChild(createTableRow(rowData));
+    tableElement.appendChild(createTableRow(rowData));
 });
+
+/* --- Exibindo informações detalhadas --- */
+
+document.getElementById("details-header").onclick = () => {
+    if(document.getElementById("details-list").style.display == 'none') {
+        document.getElementById("details-list").style.display = 'block';
+        document.getElementById("details-header").innerHTML = "<span>&#8681;</span>Informações detalhadas"
+    } else {
+        document.getElementById("details-list").style.display = 'none';
+        document.getElementById("details-header").innerHTML = "<span>&#8688;</span>Informações detalhadas"
+    }
+};
+
+document.getElementById("li-parc").innerHTML += NP;
+document.getElementById("li-taxa").innerHTML += TAX + "%";
+document.getElementById("li-vf").innerHTML += PV;
+document.getElementById("li-vv").innerHTML += PP;
+document.getElementById("li-mv").innerHTML += PB;
+document.getElementById("li-entrada").innerHTML += existeEntrada;
+
+let cf = calculateFinancingCoefficient(TAX, NP);
+
+document.getElementById("li-cf").innerHTML += cf.toFixed(4);
+document.getElementById("li-prest").innerHTML += (PV * cf).toFixed(2);
+document.getElementById("li-vp").innerHTML += "x";
+document.getElementById("li-tr").innerHTML += calculateInterestRate(NP,PV,PP,existeEntrada) * 100;
+document.getElementById("li-vc").innerHTML += "x";
 
 
 /* --- Funções auxiliares --- */
@@ -42,10 +68,27 @@ function createTableRow(rowData) {
     return tableRow
 }
 
+/**
+ * Calcula a tabela price.
+ * @func
+ * @param {number} np - Número de parcelas.
+ * @param {number} pv - Preço à vista.
+ * @param {number} pp - Preço à prazo.
+ * @param {number} t - Taxa de juros.
+ * @param {boolean} e - Indica a existência de uma parcela de entrada.
+ * @returns {Matrix} - Matriz cujas linhas são arrays da forma 
+ *                     [mês , prestação , juros , amortização , saldo devedor]
+ */
 function calculatePriceTable(np, pv, pp, t, e) {
 
+    let coefFinanc = calculateFinancingCoefficient(t,np);
+    
+    if(pp == 0.0) {
+        pp = (pv / calculateAppliedFactor(np, t, coefFinanc, e)).toFixed(2);
+    }
+
     let jurosReal = calculateInterestRate(np,pv,pp,e) * 100;
-    let pmt = (pv * calculateFinancingCoefficient(jurosReal,np)).toFixed(2);
+    let pmt = (pv * coefFinanc).toFixed(2);
     let jurosUsado = (t > 0) ? t : jurosReal.toFixed(4);
     let juros = jurosUsado;
     let jurosTotal = 0;
@@ -59,7 +102,7 @@ function calculatePriceTable(np, pv, pp, t, e) {
     if(e) np--;
 
     for(let i = 1; i <= np; i++){
-        juros = (saldoDevedor * jurosUsado / 100).toFixed;
+        juros = (saldoDevedor * jurosUsado / 100).toFixed(2);
         amortizacao = (pmt - juros).toFixed(2);
         saldoDevedor -=  amortizacao;
         saldoDevedor = saldoDevedor > 0 ? saldoDevedor.toFixed(2) : 0;
@@ -76,6 +119,21 @@ function calculatePriceTable(np, pv, pp, t, e) {
     return matrizPrice;
 }
 
+function calculateAppliedFactor(np, t, coefFinanc, e){
+    return fe(e, t) / (np * coefFinanc);
+}
+
+function fe(e, t){
+    return e ? 1+t : 1;
+}
+/**
+ * Calcula a taxa de juros real.
+ * @param {int} np
+ * @param {float} pv 
+ * @param {float} pp 
+ * @param {boolean} e 
+ * @returns {float}
+ */
 function calculateInterestRate(np,pv,pp,e) {
     const tolerancia = 0.0001;  
     let t = 0.1; // Palpite inicial
@@ -89,9 +147,7 @@ function calculateInterestRate(np,pv,pp,e) {
         t0 = t;
         funcao = calcularValorFuncao(np, pv, pp, t, e);
         derivada = calcularValorDerivadaFuncao(np, pv, pp, t, e);
-
         t = t - (funcao / derivada);
-
         i++;
     }
    
@@ -138,5 +194,5 @@ function calcularValorDerivadaFuncao(np, pv, pp, t, e){
 
 function calculateFinancingCoefficient(t, np) {
     let taxaCorrigida = t / 100;
-    return taxaCorrigida / (1 - Math.pow(1 + taxaCorrigida, (-1) * np) );
+    return taxaCorrigida / (1 - Math.pow(1+taxaCorrigida, -np) );
 }
